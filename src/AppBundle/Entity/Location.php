e5bd0c2a2d239f55ee1b47178065c31085441c41
rayane yagoubi <ryagoubi01@gmail.com>
2017-09-26 10:51:15 +0200

    add accesseurs class location

--- src/AppBundle/Entity/Location.php
+++ src/AppBundle/Entity/Location.php
@@ -44,7 +44,123 @@ class Location
 
     private $liste_vehicule;
 
+    /**
+     * Set idlocation
+     *
+     * @param integer $idlocation
+     *
+     * @return Location
+     */
+    public function setIdlocation($idlocation)
+    {
+        $this->idlocation = $idlocation;
+    
+        return $this;
+    }
 
+    /**
+     * Get idlocation
+     *
+     * @return integer
+     */
+    public function getIdlocation()
+    {
+        return $this->idlocation;
+    }
 
+    /**
+     * Set prix
+     *
+     * @param float $prix
+     *
+     * @return Location
+     */
+    public function setPrix($prix)
+    {
+        $this->prix = $prix;
+    
+        return $this;
+    }
 
+    /**
+     * Get prix
+     *
+     * @return float
+     */
+    public function getPrix()
+    {
+        return $this->prix;
+    }
+
+    /**
+     * Set dateDebut
+     *
+     * @param \DateTime $dateDebut
+     *
+     * @return Location
+     */
+    public function setDateDebut($dateDebut)
+    {
+        $this->date_debut = $dateDebut;
+    
+        return $this;
+    }
+
+    /**
+     * Get dateDebut
+     *
+     * @return \DateTime
+     */
+    public function getDateDebut()
+    {
+        return $this->date_debut;
+    }
+
+    /**
+     * Set dateFin
+     *
+     * @param \DateTime $dateFin
+     *
+     * @return Location
+     */
+    public function setDateFin($dateFin)
+    {
+        $this->date_fin = $dateFin;
+    
+        return $this;
+    }
+
+    /**
+     * Get dateFin
+     *
+     * @return \DateTime
+     */
+    public function getDateFin()
+    {
+        return $this->date_fin;
+    }
+
+    /**
+     * Set clientIdclient
+     *
+     * @param boolean $clientIdclient
+     *
+     * @return Location
+     */
+    public function setClientIdclient($clientIdclient)
+    {
+        $this->client_idclient = $clientIdclient;
+    
+        return $this;
+    }
+
+    /**
+     * Get clientIdclient
+     *
+     * @return boolean
+     */
+    public function getClientIdclient()
+    {
+        return $this->client_idclient;
+    }
 }
