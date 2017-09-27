b9d2992775fd803d024661844da6745bf96bc36f
Latamen <ait.latamene@live.fr>
2017-09-27 14:25:52 +0200

    modif projet symfony

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("test")
+     */
+    public function testAction()
+    {
+        return $this->render('index.html.twig');
+    }
 }
